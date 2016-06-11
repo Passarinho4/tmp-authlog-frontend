da0454087ba6052290e379ab0dc6c4294d3e31bf
Passarinho <passarinho4@gmail.com>
2016-06-11 23:41:57 +0200

    create user form

--- app/users/users.js
+++ app/users/users.js
@@ -49,6 +49,15 @@ angular.module('myApp')
                     data: {"userId": userId, "privileges": privileges}
                 });
             };
+            
+            $scope.createUserPopup = function (appId) {
+                ngDialog.open({
+                    template: 'users/popups/createUser.html',
+                    className: 'ngdialog-theme-default',
+                    controller: 'UsersCtrl',
+                    data: {"appId":appId}
+                });
+            };
 
             $scope.addPrivilege = function () {
 
@@ -73,5 +82,20 @@ angular.module('myApp')
                         console.log("Can't remove privilege, something went wrong.");
                     });
             };
+            
+            $scope.createUser = function () {
+                var userData = {};
+                userData.username = $scope.username;
+                userData.password = $scope.password;
+                $http.post(urls.apiUrl + "applications/" + $scope.ngDialogData.appId + "/users", userData).then(
+                    function successCallback(result) {
+                        ngDialog.close();
+                        $window.location.reload();
+                    },
+                    function failureCallback(result) {
+                        console.log("Can't create new user, something went wrong");
+                    });
+                
+            }
 
     }]);
