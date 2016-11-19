2e751301bfa8acff58d7ba15099d9501bda0fc91
Passarinho <passarinho4@gmail.com>
2016-11-19 11:38:25 +0100

    dashboard improvements

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -5,39 +5,53 @@ angular.module('myApp')
         function($scope, urls, $http, ngDialog, TokenStorage) {
 
             $scope.authenticated = TokenStorage.isAuthenticated();
+            $scope.applications = [];
+            $scope.refreshChart = refreshChart;
 
-
-            $http.get(urls.apiUrl + "applications/57d313d019388513cf91d701/hourLoginStats").then(
+            $http.get(urls.apiUrl + "applications").then(
                 function successCallback(result) {
-
-                    console.log(result.data);
-
-                    var dataset = result.data.map(function(pair) {return {x: new Date(pair.time), val_0: pair.amount}});
-
-                    console.log(dataset);
-                    $scope.data = { dataset0: dataset };
-
-                    $scope.options = {
-                        series: [
-                            {
-                                axis: "y",
-                                dataset: "dataset0",
-                                key: "val_0",
-                                label: "An area series",
-                                color: "#1f77b4",
-                                type: ['column'],
-                                id: 'mySeries0'
-                            }
-                        ],
-                        axes: {
-                            x: {key: "x", type:"date"},
-                            y: {min:0}
-
-                        }
-                    };
+                    $scope.applications = result.data;
                 },
                 function failureCallback(result) {
-                    console.log("Can't get stats from server.");
-                });
+                    console.log("Can't get applications from api.");
+                }
+            );
+
+
+            function refreshChart(appId) {
+                console.log("oopopopo");
+                $http.get(urls.apiUrl + "applications/"+appId+"/hourLoginStats").then(
+                    function successCallback(result) {
+
+                        console.log(result.data);
+
+                        var dataset = result.data.map(function(pair) {return {x: new Date(pair.time), val_0: pair.amount}});
+
+                        console.log(dataset);
+                        $scope.data = { dataset0: dataset };
+
+                        $scope.options = {
+                            series: [
+                                {
+                                    axis: "y",
+                                    dataset: "dataset0",
+                                    key: "val_0",
+                                    label: "An area series",
+                                    color: "#1f77b4",
+                                    type: ['column'],
+                                    id: 'mySeries0'
+                                }
+                            ],
+                            axes: {
+                                x: {key: "x", type:"date"},
+                                y: {min:0}
+
+                            }
+                        };
+                    },
+                    function failureCallback(result) {
+                        console.log("Can't get stats from server.");
+                    });
+            }
             
 }]);
